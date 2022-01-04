63fc1e188ee7c0b3b4087b50afdf5971c10e4f7d
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2022-01-04 17:17:41 +0100

    change to new json format

--- init_images_and_text.js
+++ init_images_and_text.js
@@ -1,49 +1,31 @@
 // @ts-check
 
-var version_number =  "v0.13.0"
-var whole_string = "Manim ".concat(version_number)
+let version_number = "v0.14.0"
+let whole_string = "version ".concat(version_number)
 
 document.getElementById('version_number').innerHTML = whole_string;
 
 
-var request1 = new XMLHttpRequest();
-request1.open("GET", "imgs/Mobjects_Basics.json", false);
+let request1 = new XMLHttpRequest();
+request1.open("GET", "imgs/gallery_parameters.json", false);
 request1.send(null);
-var jsonData1 = JSON.parse(request1.responseText);
-document.writeln(' <h1>Basic Shapes</h1>')
+
+let jsonData1 = JSON.parse(request1.responseText);
+
 for (let key of Object.keys(jsonData1)) {
-    document.write(`<img src='imgs/${key}' alt= '${jsonData1[key]}' onclick='myFunction(this);' class='image'> `);
-}
-var request2 = new XMLHttpRequest();
-request2.open("GET", "imgs/Mobjects_Text.json", false);
-request2.send(null);
-var jsonData2 = JSON.parse(request2.responseText);
-document.writeln(' <h1>Text</h1>')
-for (let key of Object.keys(jsonData2)) {
-    document.write(`<img src='imgs/${key}' alt= '${jsonData2[key]}' onclick='myFunction(this);' class='image'> `);
+    let chapter_of_html = key
+    document.writeln(`<h1> ${chapter_of_html} </h1>`)
+
+    let chapter_content = jsonData1[key]
+    for (const entery of chapter_content) {
+        let element_dict= entery[0]
+        let path_of_example = element_dict["image"]
+        let codeblock_of_example = element_dict["code"]
+        let css_of_example = element_dict["css"]
+        document.write(`<img src='imgs/${path_of_example}' data-code= '${codeblock_of_example}' onclick='display_code_from_gallery_cell(this);' class='gallery_entry' style= '${css_of_example}'>  `);
+
+    }
+
 }
 
-var request3 = new XMLHttpRequest();
-request3.open("GET", "imgs/Camera_and_Style.json", false);
-request3.send(null);
-var jsonData3 = JSON.parse(request3.responseText);
-document.writeln(' <h1>Camera and Style</h1>')
-for (let key of Object.keys(jsonData3)) {
-    document.write(`<img src='imgs/${key}' alt= '${jsonData3[key]}' onclick='myFunction(this);' class='image'> `);
-}
 
-var request4 = new XMLHttpRequest();
-request4.open("GET", "imgs/Plots.json", false);
-request4.send(null);
-var jsonData4 = JSON.parse(request4.responseText);
-document.writeln(' <h1>Plots</h1>')
-for (let key of Object.keys(jsonData4)) {
-    document.write(`<img src='imgs/${key}' alt= '${jsonData4[key]}' onclick='myFunction(this);' class='imagebig'> `);
-}
-
-// add copy button to images
-function myFunction(imgs) {
-    var name = imgs.alt;
-    navigator.clipboard.writeText(name);
-    document.getElementById("info_field").innerHTML = name + " \n<b>is now copied to clipboard.</b>"
-}
